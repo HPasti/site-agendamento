dbd0f537ea5390c81df8ff95f171c5926abfd891
HPasti <34094274+HPasti@users.noreply.github.com>
2026-06-08 08:49:00 -0300

    Update auth.php
    Atualizado o arquivo auth.php para que possa incluir a porta utilizada pelo servidor.

--- auth.php
+++ auth.php
@@ -9,12 +9,13 @@ define('DB_HOST', getenv('MYSQLHOST')     ?: 'localhost');
 define('DB_NAME', getenv('MYSQLDATABASE') ?: 'railway');
 define('DB_USER', getenv('MYSQLUSER')     ?: 'root');
 define('DB_PASS', getenv('MYSQLPASSWORD') ?: '');
+define('DB_PORT', getenv('MYSQLPORT')     ?: '3306');
 // -------------------------------------------------------
 
 function getPDO(){
     try {
         $pdo = new PDO(
-            'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+            'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4',
             DB_USER,
             DB_PASS
         );
